2788acdb273d9157569031ee147b25279ee900ba
Kurtyshanov Andrii <53937256+liayrd-tls@users.noreply.github.com>
2025-06-13 23:05:27 +0300

    base content

--- sidebars.ts
+++ sidebars.ts
@@ -22,7 +22,75 @@ const sidebars: SidebarsConfig = {
         type: "doc",
         id: "unity-client/intro",
       },
-      items: ["unity-client/installation"],
+      items: [
+        {
+          type: "category",
+          label: "1. Вступ",
+          items: [
+            "unity-client/introduction/overview",
+            "unity-client/introduction/dependencies",
+            "unity-client/introduction/initialization",
+          ],
+        },
+        {
+          type: "category",
+          label: "2. Запуск і підключення",
+          items: [
+            "unity-client/connection/server-startup",
+            "unity-client/connection/client-configuration",
+          ],
+        },
+        {
+          type: "category",
+          label: "3. Структура клієнту",
+          items: [
+            "unity-client/structure/main-components",
+            "unity-client/structure/entry-point",
+          ],
+        },
+        {
+          type: "category",
+          label: "4. Обмін данних",
+          items: [
+            "unity-client/data-exchange/data-synchronization",
+            "unity-client/data-exchange/data-filtering",
+            "unity-client/data-exchange/server-functions-burner",
+            "unity-client/data-exchange/server-functions-controller",
+            "unity-client/data-exchange/server-models",
+            "unity-client/data-exchange/client-models",
+            "unity-client/data-exchange/server-events",
+            "unity-client/data-exchange/client-events",
+          ],
+        },
+        {
+          type: "category",
+          label: "5. Меню",
+          items: ["unity-client/menu/menu-overview"],
+        },
+        {
+          type: "category",
+          label: "6. Сесія",
+          items: [
+            "unity-client/session/session-manager",
+            "unity-client/session/board-manager",
+            "unity-client/session/game-ui",
+          ],
+        },
+        {
+          type: "category",
+          label: "7. Розгортання",
+          items: [
+            "unity-client/deployment/build-creation",
+            "unity-client/deployment/build-launch",
+            "unity-client/deployment/deployment",
+          ],
+        },
+        {
+          type: "category",
+          label: "8. FAQ",
+          items: ["unity-client/faq/faq"],
+        },
+      ],
     },
   ],
   dojoServerSidebar: [
